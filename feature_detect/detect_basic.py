ebec267f52426464a9dd585ef48ab95be8727c9e
Adam Canady <adam@adamcandy.com>
2015-09-28 17:34:04 -0500

    Detect images on a photo

--- feature_detect/detect_basic.py
+++ feature_detect/detect_basic.py
@@ -1,8 +1,12 @@
 import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imread('fly.png',0)
+img = cv2.imread('medium.jpg',0)
 
 surf = cv2.SURF(400)
 kp, des = surf.detectAndCompute(img,None)
+print len(kp)
 img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
-plt.imshow(img2),plt.show()
+
+plt.imshow(img2)
+plt.show()
