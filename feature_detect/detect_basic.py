4df3c2f24eac0ec6c5b6f78cba5f6b8957d2c172
Adam Canady <adam@adamcandy.com>
2015-09-30 22:46:59 -0500

    Add cpp lib

--- feature_detect/detect_basic.py
+++ feature_detect/detect_basic.py
@@ -1,7 +1,7 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('medium.jpg',0)
+img = cv2.imread('greenej.jpg',0)
 
 surf = cv2.SURF(400)
 kp, des = surf.detectAndCompute(img,None)
@@ -9,5 +9,5 @@ print kp[0], dir(kp[0])
 print kp[0].octave, kp[0].pt, kp[0].angle, kp[0].size, kp[0].response
 img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
 
-# plt.imshow(img2)
-# plt.show()
+plt.imshow(img2)
+plt.show()
