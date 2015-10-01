3d8470b03f438a936d5013ad3391435a0e82bfc2
Adam Canady <adam@adamcandy.com>
2015-10-01 10:20:40 -0500

    Add all feature detection algorithms

--- feature_detect/detect_basic.cpp
+++ feature_detect/detect_basic.cpp
@@ -20,22 +20,66 @@ int main( int argc, char** argv )
   if( !img_1.data )
   { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 
-  //-- Step 1: Detect the keypoints using SURF Detector
-  int minHessian = 400;
-
-  Ptr<SURF> detector = SURF::create( minHessian );
-
-  std::vector<KeyPoint> keypoints_1;
-
-  detector->detect( img_1, keypoints_1 );
+  // SIFT
+  //-- Step 1: Detect the keypoints using SIFT Detector
+  int sift_minHessian = 10000;
+  Ptr<SIFT> sift_detector = SIFT::create( sift_minHessian );
+  std::vector<KeyPoint> sift_keypoints;
+  sift_detector->detect( img_1, sift_keypoints );
 
   //-- Draw keypoints
-  Mat img_keypoints_1;
-
-  drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+  Mat img_sift_keypoints;
+  drawKeypoints( img_1, sift_keypoints, img_sift_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 
   //-- Show detected (drawn) keypoints
-  imshow("Keypoints 1", img_keypoints_1 );
+  imshow("SIFT", img_sift_keypoints );
+  moveWindow("SIFT", 0, 0);
+
+  // SURF
+  //-- Step 1: Detect the keypoints using SURF Detector
+  int surf_minHessian = 400;
+  Ptr<SURF> surf_detector = SURF::create( surf_minHessian );
+  std::vector<KeyPoint> surf_keypoints;
+  surf_detector->detect( img_1, surf_keypoints );
+
+  //-- Draw keypoints
+  Mat img_surf_keypoints;
+  drawKeypoints( img_1, surf_keypoints, img_surf_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+  //-- Show detected (drawn) keypoints
+  imshow("SURF", img_surf_keypoints );
+  moveWindow("SURF", img_1.cols,0);
+
+  // ORB
+  //-- Step 1: Detect the keypoints using ORB Detector
+  int orb_minHessian = 10000;
+  Ptr<ORB> orb_detector = ORB::create( orb_minHessian );
+  std::vector<KeyPoint> orb_keypoints;
+  orb_detector->detect( img_1, orb_keypoints );
+
+  //-- Draw keypoints
+  Mat img_orb_keypoints;
+  drawKeypoints( img_1, orb_keypoints, img_orb_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+  //-- Show detected (drawn) keypoints
+  imshow("ORB", img_orb_keypoints );
+  moveWindow("ORB", img_1.cols*2, 0);
+
+  // // FAST
+  // //-- Step 1: Detect the keypoints using FAST Detector
+  // int minHessian = 10000;
+  // Ptr<FAST> detector = FAST::create( minHessian );
+  // std::vector<KeyPoint> fast_keypoints;
+  // detector->detect( img_1, fast_keypoints );
+
+  // //-- Draw keypoints
+  // Mat img_fast_keypoints;
+  // drawKeypoints( img_1, fast_keypoints, img_fast_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+  // //-- Show detected (drawn) keypoints
+  // imshow("FAST", img_fast_keypoints );
+
+
 
   waitKey(0);
 
