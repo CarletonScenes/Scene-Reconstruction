e24d858cc982a84eea642aa08dee6f308c9dd3f5
Adam Canady <adam@adamcandy.com>
2015-09-30 22:50:42 -0500

    Update path

--- feature_detect/detect_basic.py
+++ feature_detect/detect_basic.py
@@ -1,7 +1,7 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('greenej.jpg',0)
+img = cv2.imread('medium.jpg',0)
 
 surf = cv2.SURF(400)
 kp, des = surf.detectAndCompute(img,None)
