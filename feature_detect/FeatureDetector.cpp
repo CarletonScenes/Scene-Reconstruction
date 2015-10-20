79099a531f317ea4ed50b33f255c342b76a2a745
Greg Sharpe <sharpeg@carleton.edu>
2015-10-20 14:53:25 -0500

    Add featureDetector class

---
+++ feature_detect/FeatureDetector.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
+
+class FeatureDetector {
+
+public:
+    virtual void Detect(Image image, std::vector<KeyPoint>) = 0;
+}
