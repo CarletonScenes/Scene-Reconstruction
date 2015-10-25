509ebb73b7b1847d9f2aa883569c2378d533d41c
Greg Sharpe <sharpeg@carleton.edu>
2015-10-25 13:41:49 -0500

    Matches now filtered using findFundamentalMat output

--- feature_detect/FMTest.cpp
+++ feature_detect/FMTest.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include "FeatureDetectorSIFT.h"
 #include "Image.h"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/xfeatures2d/nonfree.hpp"
 
 
 using namespace cv;
@@ -10,7 +12,7 @@ using namespace cv::xfeatures2d;
 using namespace SceneComps;
 
 int main( int argc, char** argv ) {
-
+    
 	if (argc != 3) { 
 		std::cout << "Must provide filepath argument.\n";
 		return -1; 
@@ -31,7 +33,6 @@ int main( int argc, char** argv ) {
     // https://github.com/npinto/opencv/blob/master/samples/cpp/matcher_simple.cpp
     cv::Ptr<Feature2D> f2d = xfeatures2d::SIFT::create();
     
-    //SiftDescriptorExtractor extractor;
     Mat descriptors1, descriptors2; 
     f2d->compute(image1.matrix, sift_keypoints1, descriptors1);
     f2d->compute(image2.matrix, sift_keypoints2, descriptors2);
@@ -40,11 +41,45 @@ int main( int argc, char** argv ) {
     std::vector<DMatch> matches;
     matcher.match(descriptors1, descriptors2, matches);
     
+    std::vector<Point2f> ptList1;
+    std::vector<Point2f> ptList2;
+    
+    std::vector<int> queryIdxs;
+    std::vector<int> trainIdxs;
+    
+    for (std::vector<DMatch>::size_type i = 0; i != matches.size(); i++){
+        queryIdxs.push_back(matches[i].queryIdx);
+        trainIdxs.push_back(matches[i].trainIdx);
+    }
+    
+    KeyPoint::convert(sift_keypoints1, ptList1, queryIdxs);
+    KeyPoint::convert(sift_keypoints2, ptList2, trainIdxs);
+    
+    std::vector<uchar> funOut;
+    
+    //press 8 for RANSAC
+    Mat F = findFundamentalMat(ptList1, ptList2, 8, 3, .99, funOut);
+    
+    std::vector<int> funOutInt(funOut.begin(), funOut.end());
+    std::vector<DMatch> filteredMatches;
+    
+    for (std::vector<int>::size_type i = 0; i != funOut.size(); i++){
+        std::cout << funOutInt[i];
+        if (funOutInt[i]==1){
+            filteredMatches.push_back(matches[i]);
+        }
+    }
+    
+    
     // drawing the results
     namedWindow("matches", 1);
+    namedWindow("filteredMatches", 1);
     Mat img_matches;
+    Mat filtered_matches;
     drawMatches(image1.matrix, sift_keypoints1, image2.matrix, sift_keypoints2, matches, img_matches);
+    drawMatches(image1.matrix, sift_keypoints1, image2.matrix, sift_keypoints2, filteredMatches, filtered_matches);
     imshow("matches", img_matches);
+    imshow("Filtered matches", filtered_matches);
 
 	std::cout << "Press any key to exit.\n";
 	  for(;;){
