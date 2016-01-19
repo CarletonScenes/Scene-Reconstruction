f4b6df3752a12e5d5ec108a5fc46d477e8113e2a
Adam Canady <adam@adamcanady.com>
2016-01-19 11:26:40 -0600

    Add k

--- python/image.py
+++ python/image.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 import exifread
 
@@ -6,15 +7,32 @@ class Image:
         self.fname = filepath
         self.img = cv2.imread(filepath)
         # self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        self.height = self.img.shape[0]
+        self.width = self.img.shape[1]
 
-        with open(filepath, 'r') as f:
-            self.tags = exifread.process_file(f)
-
-        self.focal_length_num = self.tags['EXIF FocalLength'].values[0].num
-        self.focal_length_den = self.tags['EXIF FocalLength'].values[0].den
+        try:
+            with open(filepath, 'r') as f:
+                self.tags = exifread.process_file(f)
+            self.focal_length_num = self.tags['EXIF FocalLength'].values[0].num
+            self.focal_length_den = self.tags['EXIF FocalLength'].values[0].den
+        except:
+            self.tags = {}
+            self.focal_length_num = 1
+            self.focal_length_den = 1
 
         self.kps = None
         self.descs = None
+
+        focal_length = self.focal_length_num / self.focal_length_den
+        cx = float(self.width) / 2
+        cy = float(self.height) / 2
+        self.k = np.array([
+                [focal_length,0,cx],
+                [0,focal_length,cy],
+                [0,0,1]
+            ])
+
+        self.k_inv = np.linalg.inv(self.k)
 
     def detect_features(self):
         sift = cv2.xfeatures2d.SIFT_create()
