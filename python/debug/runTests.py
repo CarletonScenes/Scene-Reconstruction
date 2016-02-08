74290ca1fb6baee8c24f8bbf558c311aa8b52a8c
David Pickart <pickartd@carleton.edu>
2016-02-08 15:36:51 -0600

    Cleanup

--- python/debug/runTests.py
+++ python/debug/runTests.py
@@ -39,15 +39,6 @@ def triangulateFromImages(*images):
 
 def main():
 	triangulateFromImages("images/chapel1.jpg", "images/chapel2.jpg", "images/chapel3.jpg")
-    # points = triangulateTwoImages("images/chapel2.jpg", "images/chapel3.jpg")
-    # r = getArtificalR(-20)
-    # t = getArtificialTranslation(5)
-    # points2 = []
-    # for point in points:
-    #     points2.append((r.dot(point) + t.transpose())[0])
-
-    # Debug.writePointsToFile(points, "points1.ply")
-    # Debug.writePointsToFile(points2, "points2.ply")
 
 
 if __name__ == '__main__':
