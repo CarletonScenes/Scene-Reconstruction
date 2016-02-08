a5fccd55dbad77cce0d3766d6d9905cf429f0a06
Hami Abdi <abdih@carleton.edu>
2016-02-08 15:14:38 -0600

    added notes regarding common approach of two 3d lines.

--- python/debug/utils/CVFuncs.py
+++ python/debug/utils/CVFuncs.py
@@ -188,44 +188,58 @@ def triangulateFromLines(line1, line2):
 
 
 def naiveTriangulate(pts1, pts2, k, r, t):
+
+    # You want to find a common perpendicular line to both lines in 3d
+        # Write a parametric equation for each line: L1 = P1 + t (l1x, l1y, l1z); L2 = P2 + t (l2x, l2y, l2z)
+        # Find the cross product between L1 and L2, the resulting vector is the common perpendicular
+    # Now pick two random points on L1 and L2, calling them R1 and R2; find the difference between the two of them. Call this D
+    # Find the vector of D along the common perpendicular, call this D_paralle
+        # Find that as follows: Dot product D with the common perpendicular, and now multiply val of dot product by common perpendicular, call this the perp. dist.
+    # Now place this vector, which is perpendicular to L1, at R1. R1 + perp. dist.. Now to get the equation of 
+    # the new line that needs to intersect L2: (R1 + per. dist.) + t (l1x, l1y, l1z)
+    # Find the intersection between (R1 + per. dist.) + t (l1x, l1y, l1z) and P2 + t (l2x, l2y, l2z)
+    
+    
+    
+    
     # Transforms image planes by r and t, draws epipolar lines,
     # and uses those lines to triangulate points
 
-    origin1 = (0, 0, 0)
-    origin2 = (t[0][0], t[1][0], t[2][0])
+    # origin1 = (0, 0, 0)
+    # origin2 = (t[0][0], t[1][0], t[2][0])
 
-    # Image plane points (normalized and transformed)
-    imgpoints1 = []
-    imgpoints2 = []
+    # # Image plane points (normalized and transformed)
+    # imgpoints1 = []
+    # imgpoints2 = []
 
-    # IMAGE ONE
-    for point in pts1:
-        homogenous = np.append(np.array(point), [1]).transpose()
-        inv_k = np.linalg.inv(k)
-        normalized = inv_k.dot(homogenous)
-        imgpoints1.append(normalized)
+    # # IMAGE ONE
+    # for point in pts1:
+    #     homogenous = np.append(np.array(point), [1]).transpose()
+    #     inv_k = np.linalg.inv(k)
+    #     normalized = inv_k.dot(homogenous)
+    #     imgpoints1.append(normalized)
 
-    # IMAGE TWO
-    for point in pts2:
-        homogenous = np.append(np.array(point), [1]).transpose()
-        inv_k = np.linalg.inv(k)
-        normalized = inv_k.dot(homogenous)
-        transformed_point = (r.dot(normalized) + t.transpose())[0]
-        imgpoints2.append((transformed_point[0], transformed_point[1], transformed_point[2]))
+    # # IMAGE TWO
+    # for point in pts2:
+    #     homogenous = np.append(np.array(point), [1]).transpose()
+    #     inv_k = np.linalg.inv(k)
+    #     normalized = inv_k.dot(homogenous)
+    #     transformed_point = (r.dot(normalized) + t.transpose())[0]
+    #     imgpoints2.append((transformed_point[0], transformed_point[1], transformed_point[2]))
 
-    outpoints = []
+    # outpoints = []
 
     # Draw  lines and triangulate
-    count = 0
-    print len(imgpoints1)
-    for pt1, pt2 in zip(imgpoints1, imgpoints2):
-        line1 = Line(origin1, pt1)
-        line2 = Line(origin2, pt2)
-        print count
-        count += 1
-        outpoints.append(triangulateFromLines(line1, line2))
+    # count = 0
+    # print len(imgpoints1)
+    # for pt1, pt2 in zip(imgpoints1, imgpoints2):
+    #     line1 = Line(origin1, pt1)
+    #     line2 = Line(origin2, pt2)
+    #     print count
+    #     count += 1
+    #     outpoints.append(triangulateFromLines(line1, line2))
 
-    return outpoints
+    # return outpoints
 
 # reimplement: https://github.com/Itseez/opencv/blob/ddf82d0b154873510802ef75c53e628cd7b2cb13/modules/calib3d/src/triangulate.cpp#L54
 
