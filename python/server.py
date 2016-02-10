8fb17f57b36e9f0e28eb22ffca8b52c0ba4825c1
Adam Canady <adam@adamcanady.com>
2016-02-10 16:45:40 -0600

    Add working 3d render

--- python/server.py
+++ python/server.py
@@ -13,7 +13,7 @@ def submit_job():
 
 @app.route("/stream.ply")
 def view_results():
-    return app.send_static_file('debug_out.ply')
+    return app.send_static_file('blurredchapel.ply')
 
 if __name__ == "__main__":
     app.run(debug=True)
