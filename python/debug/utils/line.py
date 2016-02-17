4dd8ae134637da4558f5e74ed796a244036e5304
Greg Sharpe <sharpeg@carleton.edu>
2016-02-16 20:59:37 -0600

    normalized line's self.other also

--- python/debug/utils/line.py
+++ python/debug/utils/line.py
@@ -5,9 +5,9 @@ class Line:
 
     def __init__(self, pt1, pt2):
         self.origin = pt1
-        self.other = pt2
         self.length = ((pt2[0] - pt1[0])**2+(pt2[1] - pt1[1])**2+(pt2[2] - pt1[2])**2)**.5
         self.direction = ((pt2[0] - pt1[0])/self.length, (pt2[1] - pt1[1])/self.length, (pt2[2] - pt1[2])/self.length)
+        self.other = (pt2[0]/self.length,pt2[1]/self.length,pt2[2]/self.length)
 
     def atT(self, t):
         return (self.origin[0] + self.direction[0] * t, self.origin[1] + self.direction[1] * t, self.origin[2] + self.direction[2] * t)
